17221a95487d183f7d070e2b29129db51d0e968e
Ahmed Mater <ahmedmotair@gmail.com>
2018-12-16 06:31:00 +0200

    Fix CROS Origin in Spring Application

---
+++ Development/Training-Code-2/Backend-Services/src/main/java/com/am/training/trainingapp/configuration/CORSFilter.java
@@ -0,0 +1,22 @@
+package com.am.training.trainingapp.configuration;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext request,
+                       ContainerResponseContext response) throws IOException {
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        response.getHeaders().add("Access-Control-Allow-Headers",
+                "origin, Content-Type, Accept, authorization");
+        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        response.getHeaders().add("Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
