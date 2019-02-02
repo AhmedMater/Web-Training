65fecb2a527d27d48f7a3bd64f2319b25fa38473
Ahmed Mater <ahmedmotair@gmail.com>
2019-02-02 14:10:31 +0200

    Add Backend Code

---
+++ Development/AM-Project/Dorrar/Dorrar-Services/src/main/java/com/dorrar/DorrarApplication.java
@@ -0,0 +1,12 @@
+package com.dorrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DorrarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DorrarApplication.class, args);
+    }
+}
