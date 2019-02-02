ca79ba794101ab44de68fe87abe47f20639ec468
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-02-02 14:09:15 +0200

    Create User list

---
+++ Development/AM-Project/Dorrar/Dorrar-Services/src/main/java/com/dorrar/controller/LockupRes.java
@@ -0,0 +1,47 @@
+package com.dorrar.controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+
+public class LockupRes {
+
+    @GET
+    @Path("/colleges")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ArrayList<College> findallcollege(){
+        College college1 =new College(1,"Science");
+        College college2 =new College(2,"Nursing");
+
+        ArrayList<College> collegeArrayList=new ArrayList<>() ;
+        collegeArrayList.add(college1);
+        collegeArrayList.add(college2);
+        return collegeArrayList;
+
+    }
+    @GET
+    @Path("/university")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ArrayList<University> findalluniversity(){
+        University university1 =new University(1,"Benha");
+        University university2 =new University(2,"Cairo");
+
+        ArrayList<University> universityArrayList=new ArrayList<>();
+        universityArrayList.add(university1);
+        universityArrayList.add(university2);
+        return universityArrayList;
+
+    }
+    public ArrayList<Country> findallcountry(){
+        Country country1=new Country(1,"Egypt");
+        Country country2=new Country(2,"sudan");
+
+        ArrayList<Country> countryArrayList=new ArrayList<>();
+        countryArrayList.add(country1);
+        countryArrayList.add(country2);
+        return countryArrayList;
+    }
+
+}
