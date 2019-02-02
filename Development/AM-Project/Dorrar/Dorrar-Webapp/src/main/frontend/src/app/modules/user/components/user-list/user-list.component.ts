c0ddd37079e40594839d321304f38ab172471874
ahmedfathy97 <43169681+ahmedfathy97@users.noreply.github.com>
2019-02-02 04:18:30 -0800

    comment user-list

--- Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/modules/user/components/user-list/user-list.component.ts
+++ Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/modules/user/components/user-list/user-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 //import {FilterUserData} from "../../shared/filter-user.data";
 import {UserServices} from "../../shared/user.service";
+import {FilterUserData} from "../../shared/data/user-filter.data";
 //import {College} from "../../shared/college";
 
 @Component({
@@ -11,53 +12,53 @@ import {UserServices} from "../../shared/user.service";
   providers:[FormBuilder,UserServices]
 })
 export class UserFilterComponent implements OnInit {
-  formData :FormGroup=this.formBuilder.group({
-    fullname:[null,[Validators.required,Validators.maxLength(20)]],
-    college:[null,[Validators.required]],
-    university:[null,[Validators.required]],
-    country:[null,[Validators.required]],
-    birthdatefrom:[null,[Validators.required]],
-    birthdateto:[null,[Validators.required]],
-  })
-  //public collegeArrayList :College[];
-  constructor(private formBuilder:FormBuilder ,private userServices:UserServices) { }
-
+  // formData :FormGroup=this.formBuilder.group({
+  //   fullname:[null,[Validators.required,Validators.maxLength(20)]],
+  //   college:[null,[Validators.required]],
+  //   university:[null,[Validators.required]],
+  //   country:[null,[Validators.required]],
+  //   birthdatefrom:[null,[Validators.required]],
+  //   birthdateto:[null,[Validators.required]],
+  // })
+  // //public collegeArrayList :College[];
+  // constructor(private formBuilder:FormBuilder ,private userServices:UserServices) { }
+  //
   ngOnInit() {
 
   }
-  filter(){
-    let data : FilterUserData =new FilterUserData();
-    data.fullname=this.formData.get('fullname').value;
-    data.college=this.formData.get('college').value;
-    data.university=this.formData.get('university').value;
-    data.country=this.formData.get('country').value;
-    data.birthdatefrom=this.formData.get('birthdatefrom').value;
-    data.birthdateto=this.formData.get('birthdateto').value;
-
-
-    console.log(data);
-
-    this.userServices.filter(data).subscribe(
-      res=>
-      {
-        console.log('Request Succeeded');
-      },
-      err=>
-      {
-        console.log(err);
-      }
-    );
-  }
-  findallcollege(){
-    // let data:College=new College();
-    // data.id=this.formData.get('id').value;
-    // data.labelEN=this.formData.get('labelEN').value;
-
-    this.userServices.findallcollege().subscribe(res =>
-    {
-      console.log(res);
-      this.collegeArrayList=res;
-    })
-  }
+  // filter(){
+  //   let data : FilterUserData =new FilterUserData();
+  //   data.fullname=this.formData.get('fullname').value;
+  //   data.college=this.formData.get('college').value;
+  //   data.university=this.formData.get('university').value;
+  //   data.country=this.formData.get('country').value;
+  //   data.birthdatefrom=this.formData.get('birthdatefrom').value;
+  //   data.birthdateto=this.formData.get('birthdateto').value;
+  //
+  //
+  //   console.log(data);
+  //
+  //   this.userServices.filter(data).subscribe(
+  //     res=>
+  //     {
+  //       console.log('Request Succeeded');
+  //     },
+  //     err=>
+  //     {
+  //       console.log(err);
+  //     }
+  //   );
+  // }
+  // findallcollege(){
+  //   // let data:College=new College();
+  //   // data.id=this.formData.get('id').value;
+  //   // data.labelEN=this.formData.get('labelEN').value;
+  //
+  //   this.userServices.findallcollege().subscribe(res =>
+  //   {
+  //     console.log(res);
+  //     this.collegeArrayList=res;
+  //   })
+  // }
 
 }
