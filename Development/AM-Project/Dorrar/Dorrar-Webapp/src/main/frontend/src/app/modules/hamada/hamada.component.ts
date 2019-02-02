2439af2ef0a0657f2bcbbff66e80df05a334bd8d
ahmedfathy97 <43169681+ahmedfathy97@users.noreply.github.com>
2019-02-02 02:56:40 -0800

    Remove .git folder

---
+++ Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/modules/hamada/hamada.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hamada',
+  templateUrl: './hamada.component.html',
+  styleUrls: ['./hamada.component.scss']
+})
+export class HamadaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
