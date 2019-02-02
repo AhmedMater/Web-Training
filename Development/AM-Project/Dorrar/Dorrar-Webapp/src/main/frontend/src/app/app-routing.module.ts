88fa7012fd55a56f72d4bf8915b25783e34a46d0
Hala mahmoud <43175109+halamahmoud8@users.noreply.github.com>
2019-02-02 15:06:29 +0200

    Create Course Reference Component

--- Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/app-routing.module.ts
+++ Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/app-routing.module.ts
@@ -3,17 +3,21 @@ import { Routes, RouterModule } from '@angular/router';
 import {SimpleLayoutComponent} from "./layout/components/simple-layout/simple-layout.component";
 import {MainLayoutComponent} from "./layout/components/main-layout/main-layout.component";
 import {HomeComponent} from "./modules/home/home.component";
+import {CreateReferenceComponent} from "./modules/course/component/create-reference/create-reference.component";
 // import * as path from "path";
 import {LoginComponent} from "./modules/security/components/login/login.component";
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: '', component: SimpleLayoutComponent, children:[
-      {path: 'login', component: LoginComponent}
+      {path: 'login', component: LoginComponent},
+      {path: 'reference', component: CreateReferenceComponent},
+
     ]},
   {
     path: '', component: MainLayoutComponent, children: [
-      {path:'home', component: HomeComponent}
+      {path:'home', component: HomeComponent},
+
     ]
   }
 ];
