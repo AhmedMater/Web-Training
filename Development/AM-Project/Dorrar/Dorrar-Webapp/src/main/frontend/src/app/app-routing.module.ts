d656fdbd27de0b16136b0ea912bee6e433d890bf
ayaasamir <43170785+ayaasamir@users.noreply.github.com>
2019-02-02 15:01:12 +0200

    Create Login Security Component

--- Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/app-routing.module.ts
+++ Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {SimpleLayoutComponent} from "./layout/components/simple-layout/simple-layout.component";
 import {MainLayoutComponent} from "./layout/components/main-layout/main-layout.component";
 import {HomeComponent} from "./modules/home/home.component";
+// import * as path from "path";
+import {LoginComponent} from "./modules/security/components/login/login.component";
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
-  {path: '', component: SimpleLayoutComponent},
+  {path: '', component: SimpleLayoutComponent, children:[
+      {path: 'login', component: LoginComponent}
+    ]},
   {
     path: '', component: MainLayoutComponent, children: [
       {path:'home', component: HomeComponent}
