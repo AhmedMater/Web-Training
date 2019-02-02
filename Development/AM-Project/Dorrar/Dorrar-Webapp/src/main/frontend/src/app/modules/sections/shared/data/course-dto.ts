8b03472a70d90260b072771f025cc0cda228e4ca
YaraAbdElrahman <43352276+YaraAbdElrahman@users.noreply.github.com>
2019-02-02 12:47:15 +0200

    create section page component

---
+++ Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/modules/sections/shared/data/course-dto.ts
@@ -0,0 +1,9 @@
+// import {sectionData} from "./SectionData";
+//
+// //TODO: rename filename to course-dto
+// export class CourseDto {
+//   //TODO: rename  sectionData to SectionData
+//   //TODO: rename  item to sectionData
+//   item : sectionData[] = [];
+//
+// }
