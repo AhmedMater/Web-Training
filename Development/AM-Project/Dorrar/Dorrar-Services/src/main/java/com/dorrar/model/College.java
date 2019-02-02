28ce84eae6b954b9beb0bcc776571490b2372711
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-02-02 14:10:13 +0200

    Create User list

---
+++ Development/AM-Project/Dorrar/Dorrar-Services/src/main/java/com/dorrar/model/College.java
@@ -0,0 +1,39 @@
+package com.dorrar.model;
+
+public class College {
+    private int id;
+    private String labelEN;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLabelEN() {
+        return labelEN;
+    }
+
+    public void setLabelEN(String labelEN) {
+        this.labelEN = labelEN;
+    }
+
+    public College(){
+
+    }
+
+    public College(int id, String labelEN) {
+        this.id = id;
+        this.labelEN = labelEN;
+    }
+
+    @Override
+    public String toString() {
+        return "College{" +
+                "id=" + id +
+                ", labelEN='" + labelEN + '\'' +
+                '}';
+    }
+}
