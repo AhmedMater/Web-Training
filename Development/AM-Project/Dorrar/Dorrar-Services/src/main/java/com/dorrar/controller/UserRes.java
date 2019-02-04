15b232b07dca07db723324f0dd59dd6588539960
Hala mahmoud <43175109+halamahmoud8@users.noreply.github.com>
2019-02-04 19:10:41 +0200

    Create Course Reference Files

--- Development/AM-Project/Dorrar/Dorrar-Services/src/main/java/com/dorrar/controller/UserRes.java
+++ Development/AM-Project/Dorrar/Dorrar-Services/src/main/java/com/dorrar/controller/UserRes.java
@@ -15,34 +15,6 @@ import java.util.ArrayList;
 
 @Path("/user")
 public class UserRes {
-    @POST
-    @Path("/reference")
-    @Consumes(MediaType.APPLICATION_JSON)
-    //TODO: use Course Data Class as payload instead of Course Reference
-    //TODO: rename Function to be addCourseReferences
-    public void reference(CourseReference data) {
-        System.out.println("recieved is success");
-        System.out.println(data.toString());
-
-
-    }
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/findData")
-    public ArrayList<CorRefType> findData() {
-        //TODO: Create Class CorRefType has 2 Attributes (id, labelEN)
-        //TODO: Create CorRefType List then add 2 Objects (1, Course) (2, Book) and return the List
-        //TODO: Collections List & ArrayList
-
-        ArrayList<CorRefType> list = new ArrayList<CorRefType>();
-        list.add(new CorRefType(1, "Course"));
-        list.add(new CorRefType(2, "Book"));
-
-        return list;
-
-
-    }
 
 
 
