2dd5192ed4252b90d6943854ccd0cbc88a491891
Hala mahmoud <43175109+halamahmoud8@users.noreply.github.com>
2019-02-04 19:07:04 +0200

    Create Course Reference Files

--- Development/AM-Project/Dorrar/Dorrar-Services/src/main/resources/SQL-Script/Clean-Structure.sql
+++ Development/AM-Project/Dorrar/Dorrar-Services/src/main/resources/SQL-Script/Clean-Structure.sql
@@ -1,1 +1,41 @@
 CREATE SCHEMA `dorrar` ;
+CREATE TABLE `dorrar`.`course` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  PRIMARY KEY (`id`));
+CREATE TABLE `dorrar`.`reference` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `course-id` INT NOT NULL,
+  `ref-type-id` INT NOT NULL,
+  `name` VARCHAR(50) NOT NULL,
+  `url` VARCHAR(100) NOT NULL,
+  `type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  INDEX `fk-reference-course_idx` (`course-id` ASC),
+  CONSTRAINT `fk-reference-course`
+    FOREIGN KEY (`course-id`)
+    REFERENCES `dorrar`.`course` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+    CREATE TABLE `dorrar`.`ref-type` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `label-en` VARCHAR(45) NOT NULL,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  PRIMARY KEY (`id`));
+ALTER TABLE `dorrar`.`reference`
+ADD INDEX `fk-reference-ref-type_idx` (`ref-type-id` ASC);
+
+
+ALTER TABLE `dorrar`.`reference`
+ADD CONSTRAINT `fk-reference-ref-type`
+  FOREIGN KEY (`ref-type-id`)
+  REFERENCES `dorrar`.`ref-type` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+
+
+
+
