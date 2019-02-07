0b30d0b2ddc7c0e09d65076d5fb936fbee7417ef
Ahmed Mater <ahmedmotair@gmail.com>
2019-02-07 19:21:41 +0200

    Revert: Move section component to course folder

---
+++ Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/modules/course/shared/course.service.ts
@@ -0,0 +1,34 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {CorRefType} from "./data/cor-ref-type-dto.data";
+import {CourseData} from "./data/course-data-dto.data";
+
+@Injectable()
+export class CourseService {
+
+  constructor(private http:HttpClient) { }
+
+  saveCorReferences(data:CourseData) {
+    //TODO: Hala - Use App_base_url such as in User Service
+    return this.http.post("http://localhost:8080/api/course/reference", data);
+  }
+  //   saveCorReferences(data:CourseData) {
+  //   return this.http.post("http://localhost:8080/api/course/reference", data)
+  //
+  //
+  // }
+
+  //TODO: Hala - Use App_base_url such as in User Service
+  findCorRefTypes() {
+    return this.http.get<CorRefType[]>("http://localhost:8080/api/course/findData")
+
+  }
+  //TODO: Yara - Use App_base_url such as in User Service
+
+  addNewSections( data : CourseData) {
+    return this.http.post( this.BASE_URL +"/1/section", data);
+
+  }
+
+
+}
