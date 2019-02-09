bed8f58397b510ef84fc543d94bf34216a2f722e
Yousef El-hadad <38881626+yousefelhadad@users.noreply.github.com>
2019-02-09 04:44:56 +0200

    Rename CourseData

--- Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/modules/course/shared/data/course-dto.data.ts
+++ Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/modules/course/shared/data/course-dto.data.ts
@@ -1,16 +1,18 @@
+import {CourseReference} from "./course-ref-dto.data";
 import {SectionData} from "./section-data";
-import {CourseRefDTO} from "./course-ref-dto.data";
+import {CorMainDetail} from "./cor-main-detail.data";
 
 
 //TODO: Youssef - rename class to CourseDTO
 //TODO: Youssef - rename file to course-dto.data
-export class CourseData {
+export class CourseDTO {
   //TODO: Hala - should be references
-  references: CourseRefDTO[] = [];
+  references: CourseReference[] = [];
 
   //TODO: Yara - should be sections
   sections : SectionData[] = [];
 
   //TODO: Youssef - should have mainDetails Object here
+  detail : CorMainDetail ;
 }
 
