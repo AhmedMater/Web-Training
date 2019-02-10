4202bcb29811241c216ec05f33d5495a2918949e
Ahmed Mater <ahmedmotair@gmail.com>
2019-02-10 04:44:43 +0200

    Comment Authorization Frontend

--- Development/AM-Project/Dorrar/Dorrar-Services/src/main/java/com/dorrar/repository/LookupRep.java
+++ Development/AM-Project/Dorrar/Dorrar-Services/src/main/java/com/dorrar/repository/LookupRep.java
@@ -48,29 +48,31 @@ public class LookupRep {
         return rolePages;
     }
 
-    public List<Action> getUserActions(int userID)
-    {
-       // userID= 1 ;
-       String sql ="SELECT aa.id , aa.label_en as user_actions FROM auth_user_action ua\n" +
-               "LEFT JOIN auth_user au ON ua.user_id = au.id \n" +
-               "LEFT JOIN auth_action aa ON ua.action_id = aa.id \n" +
-               "WHERE ua.user_id ="+userID+";" ;
-       RowMapper<Action> userActionRM =new UserActionsRM() ;
-       List<Action> userActionsList =this.jdbcTemplate.query(sql ,userActionRM) ;
-       return userActionsList ;
+    //TODO: Fathy - These Functions should be in UserRep
+    public List<Action> getUserActions(int userID) {
+        // userID= 1 ;
+        String sql = "SELECT aa.id , aa.label_en as user_actions FROM auth_user_action ua" +
+                "LEFT JOIN auth_user au ON ua.user_id = au.id " +
+                "LEFT JOIN auth_action aa ON ua.action_id = aa.id " +
+                //TODO: Fathy - We pass Parameters using queryForObject function
+                "WHERE ua.user_id =" + userID + ";";
+        RowMapper<Action> userActionRM = new UserActionsRM();
+        List<Action> userActionsList = this.jdbcTemplate.query(sql, userActionRM);
+        return userActionsList;
     }
 
 
-    public List<Page> getUserPage(int userID)
-    {
-       // userID =1 ;
-        String sql ="SELECT ap.id ,   ap.label_en as user_pages FROM auth_user_page up\n" +
-                "LEFT JOIN auth_user au ON up.user_id = au.id \n" +
-                "LEFT JOIN auth_page ap ON up.page_id = ap.id \n" +
-                "WHERE up.user_id ="+userID+";" ;
-        RowMapper<Page> userPageRM =new UserPageRM() ;
-        List<Page> userPagesList = this.jdbcTemplate.query(sql ,userPageRM) ;
-        return userPagesList ;
+    //TODO: Fathy - These Functions should be in UserRep
+    public List<Page> getUserPage(int userID) {
+        // userID =1 ;
+        String sql = "SELECT ap.id ,   ap.label_en as user_pages FROM auth_user_page up" +
+                "LEFT JOIN auth_user au ON up.user_id = au.id " +
+                "LEFT JOIN auth_page ap ON up.page_id = ap.id " +
+                //TODO: Fathy - We pass Parameters using queryForObject function
+                "WHERE up.user_id =" + userID + ";";
+        RowMapper<Page> userPageRM = new UserPageRM();
+        List<Page> userPagesList = this.jdbcTemplate.query(sql, userPageRM);
+        return userPagesList;
     }
 
 }
--- Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/infrastructure/directives/authorization/auth-action.directive.ts
+++ Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/infrastructure/directives/authorization/auth-action.directive.ts
@@ -1,40 +1,40 @@
-// import {Directive, Input, OnInit, TemplateRef, ViewContainerRef} from "@angular/core";
-// import {LocalStorageService} from "../../../services/local-storage.service";
-// import {AuthActions} from "./data/auth-action.data";
-//
-// @Directive({
-//   selector: '[authorizeAction]'
-// })
-// export class AuthorizeActionDirective implements OnInit {
-//   authActions: string [];
-//
-//   constructor(
-//     private templateRef: TemplateRef<any>,
-//     private viewContainerRef: ViewContainerRef,
-//     private localStorageService: LocalStorageService) {
-//   }
-//
-//   ngOnInit(): void {
-//   }
-//
-//   @Input()
-//   set authorizeAction(actionList: AuthActions[]) {
-//     if (actionList != undefined && actionList != null && actionList.length != 0) {
-//       // this.authActions = this.localStorageService.getUserActions();
-//       let found = false;
-//
-//       for(let i=0; i<actionList.length; i++) {
-//         if (this.authActions.includes(actionList[i])) {
-//           found = true;
-//           break;
-//         }else
-//           this.viewContainerRef.clear();
-//       }
-//
-//       if (found)
-//         this.viewContainerRef.createEmbeddedView(this.templateRef);
-//       else
-//         this.viewContainerRef.clear();
-//     }
-//   }
-// }
+import {Directive, Input, OnInit, TemplateRef, ViewContainerRef} from "@angular/core";
+import {AuthActions} from "./data/auth-action.data";
+import {LocalStorageService} from "../../../layout/shared/services/local-storage.service";
+
+@Directive({
+  selector: '[authorizeAction]'
+})
+export class AuthorizeActionDirective implements OnInit {
+  authActions: string [];
+
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainerRef: ViewContainerRef,
+    private localStorageService: LocalStorageService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  @Input()
+  set authorizeAction(actionList: AuthActions[]) {
+    if (actionList != undefined && actionList != null && actionList.length != 0) {
+      // this.authActions = this.localStorageService.getUserActions();
+      let found = false;
+
+      for(let i=0; i<actionList.length; i++) {
+        if (this.authActions.includes(actionList[i])) {
+          found = true;
+          break;
+        }else
+          this.viewContainerRef.clear();
+      }
+
+      if (found)
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
+      else
+        this.viewContainerRef.clear();
+    }
+  }
+}
