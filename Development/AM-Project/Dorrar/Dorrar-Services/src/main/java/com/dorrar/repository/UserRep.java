545668e61941b8148f5bd9faaf5739863cd9242c
Ahmed Mater <ahmedmotair@gmail.com>
2019-02-11 19:53:57 +0200

    Add Repository Annotation

--- Development/AM-Project/Dorrar/Dorrar-Services/src/main/java/com/dorrar/repository/UserRep.java
+++ Development/AM-Project/Dorrar/Dorrar-Services/src/main/java/com/dorrar/repository/UserRep.java
@@ -7,9 +7,11 @@ import com.dorrar.model.UserPageRM;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class UserRep {
     private final JdbcTemplate jdbcTemplate;
 
