86a16996b01b8f43b51872a520e313a926b64c44
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-02-13 15:36:07 +0200

    Create User list

---
+++ Development/AM-Project/Dorrar/Dorrar-Services/src/main/java/com/dorrar/model/UserVTO.java
@@ -0,0 +1,18 @@
+package com.dorrar.model;
+
+import java.io.Serializable;
+
+public class UserVTO implements Serializable {
+    private String fullName;
+
+    public UserVTO() {
+    }
+
+    public String getFullName(String s) {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
