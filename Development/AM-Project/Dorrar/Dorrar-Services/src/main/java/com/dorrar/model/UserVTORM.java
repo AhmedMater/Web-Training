3850e4dbe824f43a71fe7ada1c5b4e5f9da81017
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-02-13 15:37:28 +0200

    Create User list

---
+++ Development/AM-Project/Dorrar/Dorrar-Services/src/main/java/com/dorrar/model/UserVTORM.java
@@ -0,0 +1,17 @@
+package com.dorrar.model;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserVTORM implements RowMapper<UserVTO> {
+    @Override
+    public UserVTO mapRow(ResultSet rs, int rowIndex) throws SQLException {
+       UserVTO data=new UserVTO();
+       String firstName=rs.getString("first_name");
+       String lastName=rs.getString("last_name");
+       data.setFullName(firstName + " " +lastName);
+        return data;
+    }
+}
