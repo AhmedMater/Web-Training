a603e860b11dbff3f23ee65b4ddf9546a9acb220
manar elsayed <43217523+manar2018@users.noreply.github.com>
2019-02-15 12:16:43 +0200

    Create User List

--- Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/modules/user/shared/user.service.ts
+++ Development/AM-Project/Dorrar/Dorrar-Webapp/src/main/frontend/src/app/modules/user/shared/user.service.ts
@@ -7,43 +7,44 @@ import {Country} from "./data/country";
 import {Page} from "./data/page";
 import {Role} from "./data/role";
 import {ConfigParam} from "../../../infrastructure/common/config-param";
+import {UserVTO} from "./data/user-vto.data";
 
 
 @Injectable()
 export class UserService {
 
   BASE_URL: string = ConfigParam.APP_BASE_URL + "/lookup";
-  constructor(private httpClient:HttpClient){
+  constructor(private http:HttpClient){
   }
   getRoles()
   {
-    return this.httpClient.get<Role[]>(this.BASE_URL + '/roles' );
+    return this.http.get<Role[]>(this.BASE_URL + '/roles' );
   }
 
   //TODO: function labelEN to be camelCase
   getPages()
   {
-    return this.httpClient.get<Page[]>(
+    return this.http.get<Page[]>(
       this.BASE_URL + '/page'
     ) ;
   }
 
   filter(data:UserList){
-    return this.httpClient.post("http://localhost:8080/api/user/filter",data);
+    return this.http.post("http://localhost:8080/api/user/filter",data);
 
   }
 
     findallcollege(){
-     return this.httpClient.get<College[]>("http://localhost:8080/api/user/colleges");
+     return this.http.get<College[]>("http://localhost:8080/api/user/colleges");
 
    }
 
    findalluniversity(){
-    return this.httpClient.get<University[]>("http://localhost:8080/api/user/university")
+    return this.http.get<University[]>("http://localhost:8080/api/user/university")
    }
 
    findallcountry(){
-    return this.httpClient.get<Country[]>("http://localhost:8080/api/user/country")
+    return this.http.get<Country[]>("http://localhost:8080/api/user/country")
    }
 
   findAll(){
